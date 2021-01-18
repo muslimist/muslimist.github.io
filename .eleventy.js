bd1ac39385615f48ed22a5e4db0c61ed8d797709
Rawhi Mohammad <royjr96@gmail.com>
2021-01-18 11:19:47 -0500

    fixed twa asset link issue

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 module.exports = function(eleventyConfig) {
+  // not working, copied manually
+  //eleventyConfig.addPassthroughCopy({ "src/.wellknown": ".wellknown" });
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
-  eleventyConfig.addPassthroughCopy({ "src/.wellknown": ".wellknown" });
-  eleventyConfig.addPassthroughCopy({ "CNAME": "CNAME" });
+  eleventyConfig.addPassthroughCopy({ "src/_config.yml": "_config.yml" });
+  eleventyConfig.addPassthroughCopy({ "src/CNAME": "CNAME" });
   eleventyConfig.addPassthroughCopy({ "src/manifest.webmanifest": "manifest.webmanifest" });
   eleventyConfig.addPassthroughCopy({ "src/service-worker.js": "service-worker.js" });
-  eleventyConfig.addPassthroughCopy({ "src/_config.yml": "_config.yml" });
+  
   return {
     dir: {
       input: "src",
