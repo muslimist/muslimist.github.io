c6cdf023e2ee8212248821c68baa90bbd4a0f099
Rawhi Mohammad <royjr96@gmail.com>
2021-01-18 02:26:57 -0500

    move files

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/.wellknown": ".wellknown" });
   eleventyConfig.addPassthroughCopy({ "CNAME": "CNAME" });
   eleventyConfig.addPassthroughCopy({ "src/manifest.webmanifest": "manifest.webmanifest" });
   eleventyConfig.addPassthroughCopy({ "src/service-worker.js": "service-worker.js" });
+  eleventyConfig.addPassthroughCopy({ "src/_config.yml": "_config.yml" });
   return {
     dir: {
       input: "src",
