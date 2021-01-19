1e890cf5a8930076e51260f0b790ecbcacb6ae86
Rawhi Mohammad <royjr96@gmail.com>
2021-01-18 23:07:16 -0500

    stuff

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   // not working, copied manually
   //eleventyConfig.addPassthroughCopy({ "src/.wellknown": ".wellknown" });
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/quran": "quran" });
   eleventyConfig.addPassthroughCopy({ "src/_config.yml": "_config.yml" });
   eleventyConfig.addPassthroughCopy({ "src/CNAME": "CNAME" });
   eleventyConfig.addPassthroughCopy({ "src/manifest.webmanifest": "manifest.webmanifest" });
