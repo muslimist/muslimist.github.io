0aa8f5633bcbf7a691b01ab69608e2a596d597be
Rawhi Mohammad <royjr96@gmail.com>
2021-01-20 20:31:58 -0500

    fixed 404

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
   //eleventyConfig.addPassthroughCopy({ "src/.wellknown": ".wellknown" });
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/quran": "quran" });
+  eleventyConfig.addPassthroughCopy({ "src/404.html": "404.html" });
   eleventyConfig.addPassthroughCopy({ "src/_config.yml": "_config.yml" });
   eleventyConfig.addPassthroughCopy({ "src/CNAME": "CNAME" });
   eleventyConfig.addPassthroughCopy({ "src/manifest.webmanifest": "manifest.webmanifest" });
